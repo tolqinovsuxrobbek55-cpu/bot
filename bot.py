5b5f29d9e1b1f3cfc9c1b0ce86962bbef0819505
Suxrob <tolqinovsuxrobbek55@email.com>
2026-08-12 06:21:15 +0000

    Add help command to bot.py in morfin branch

--- bot.py
+++ bot.py
@@ -4,8 +4,8 @@ import telebot
 TOKEN = "8948036879:AAFSVUCtCv3L58z-JGPCkA8yxEup-5qGPks"
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Salom! Men botman.")
+    bot.reply_to(message, "Bu yordam matni!.")
 
 bot.polling()
